2800a38b3cb991f2d9123b2d76e28b428cdcf7d8
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2024-11-17 21:19:55 +0000

    Add lines to bet on function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+MAX_LINES = 3
+
+
 
 def deposit():
     while True:
-        amount = input("How much would you like to deposite? £")
+        amount = input("How much would you like to deposite? £ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -10,6 +13,30 @@ def deposit():
                 print("Amount must be greater than 0.")
         else:
             print("Please enter a number")
-            
+
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines you would like to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number")
+
+    return lines
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+
+main()
 
